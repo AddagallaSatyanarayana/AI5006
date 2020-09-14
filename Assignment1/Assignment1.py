2a7da785c321c3328eea74e6c0265275e6f59511
AddagallaSatyanarayana <70761557+AddagallaSatyanarayana@users.noreply.github.com>
2020-09-14 19:08:58 +0530

    Add files via upload

--- Assignment1/Assignment1.py
+++ Assignment1/Assignment1.py
@@ -1,64 +1,48 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Sep  5 17:43:17 2020
-
 @author: Satyanarayana
 """
-
 import numpy as np
 import matplotlib.pyplot as plt
 
 fig = plt.figure()
 ax = plt.axes(projection="3d")
 
-# For plotting the 3 lines
-def plot_lines(x,y,z,k):
+def plot_lines(x,y,z,k,l):
 
-   ax.plot3D(x, y, z, k)
+   ax.plot3D(x, y, z, k,label=l)
 
+# l1_dir and l2_dir   
+l1_dir = np.array([3, -16, 7])
+l2_dir = np.array([3, 8, -5])
+perpendicular_dir = np.array([2, 4, 6])
 
-# direction of line1 and line2   
-line1 = np.array([3, -16, 7])
-line2 = np.array([3, 8, -5])
+#points through which the lines pass
+point1 = np.array([8,-19,10])
+point2 = np.array([15,29,5])
+point3 = np.array([1,2,-4])
 
-#compute cross product for perpendicular
-output1 = np.cross(line1, line2)
-line3 = output1/np.linalg.norm(output1)
-
-#point through which the line passes
-point = np.array([1,2,-4])
-
-
-#x1,y1,z1 are points on line1   
-#x2,y2,z2 are points on line2 
-#x3,y3,z3 are points on line3
-x1 = []
-y1 = []
-z1 = []
-x2 = []
-y2 = []
-z2 = []
-x3 = []
-y3 = []
-z3 = []
+#x1,y1,z1 are points on l1   
+#x2,y2,z2 are points on l2 
+#x3,y3,z3 are points on lper
+x1 = [];y1 = [];z1 = []
+x2 = [];y2 = [];z2 = []
+x3 = [];y3 = [];z3 = []
 
 #Compute points on 3 lines for plotting
 for i in range(-10,20):
     
-    x1 +=[line1[0]*i+8]
-    y1 +=[line1[1]*i-19]
-    z1 +=[line1[2]*i+10]
-    x2 +=[line2[0]*i+15]
-    y2 +=[line2[1]*i+29]
-    z2 +=[line2[2]*i+5]
-    x3 +=[line3[0]*i*10+1]
-    y3 +=[line3[1]*i*10+2]
-    z3 +=[line3[2]*i*10-4]
+    x1 +=[l1_dir[0]*i+point1[0]];y1 +=[l1_dir[1]*i+point1[1]]; z1 +=[l1_dir[2]*i+point1[2]]
+    x2 +=[l2_dir[0]*i+point2[0]];y2 +=[l2_dir[1]*i+point2[1]];z2 +=[l2_dir[2]*i+point2[2]]
+    x3 +=[perpendicular_dir[0]*i+point3[0]]; y3 +=[perpendicular_dir[1]*i+point3[1]]; z3 +=[perpendicular_dir[2]*i-point3[2]]
     
-plot_lines(x1,y1,z1,'gray')
-plot_lines(x2,y2,z2,'green')
-plot_lines(x3,y3,z3,'red')
-    
-ax.scatter(point[0],point[1],point[2])
+ax.set_xlabel('x axis')
+ax.set_ylabel('y axis')
+ax.set_zlabel('z axis')
 
+plot_lines(x1,y1,z1,'gray','line1')
+plot_lines(x2,y2,z2,'green','line2')
+plot_lines(x3,y3,z3,'red','perpendicular')
+ax.legend()
+ax.scatter(point3[0],point3[1],point3[2])    
 plt.show()
